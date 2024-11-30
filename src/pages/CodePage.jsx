cfe5a1c154a2d0639a3b9ef9544312968fbb0cf2
Nir Fakiro <fakironir@gmail.com>
2024-11-30 11:22:21 +0200

    Improve status bar

--- src/pages/CodePage.jsx
+++ src/pages/CodePage.jsx
@@ -1,21 +1,44 @@
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 import Editor from '@monaco-editor/react'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Modal from '@mui/material/Modal'
+
 import { loadCode } from '../store/actions/code.action'
 import { socketService } from '../services/socket.service'
 
 export function CodePage() {
   const { id } = useParams()
   const code = useSelector((state) => state.codeModule.code)
+  const navigte = useNavigate()
+  const handleClose = () => setOpen(false)
 
+  const [open, setOpen] = React.useState(false)
+  const [modalContent, setModalContent] = useState({
+    text: '',
+    icon: '',
+  })
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  }
   const [role, setRole] = useState('')
   const [roomStatus, setRoomStatus] = useState({
     isStudentPresent: false,
     watcherCount: 0,
   })
-  const navigte = useNavigate()
 
   const [userCode, setUserCode] = useState('')
   const [solution, setSolution] = useState(false)
@@ -29,11 +52,15 @@ export function CodePage() {
 
     socketService.on('user-role', setRole)
     socketService.on('room-status', setRoomStatus)
+    socketService.on('code-changed', (newCode) => {
+      setUserCode(newCode)
+    })
 
     return () => {
       socketService.emit('disconnect-from-room')
       socketService.off('user-role')
-      socketService.off('watcher-count')
+      socketService.off('room-status')
+      socketService.off('code-changed')
     }
   }, [id])
 
@@ -42,10 +69,17 @@ export function CodePage() {
   }
 
   function handleEditorChange(value) {
+    console.log('vale:', value)
+
     setUserCode(value)
+    console.log('usercode:', userCode)
   }
+  socketService.emit('code-update', id, userCode)
 
   function onBackToLobby() {
+    socketService.on('redirect-to-lobby', () => {
+      navigte('/')
+    })
     socketService.emit('disconnect-from-room')
     navigte('/')
   }
@@ -56,8 +90,17 @@ export function CodePage() {
 
     try {
       // Convert the user's code string into an executable function using eval.
+      console.log('user-code:', userCode)
+
       const userFunction = eval(`(${userCode})`)
 
+      if (typeof userFunction !== 'function') {
+        setModalContent({
+          text: 'The code is not a valid function. Please review it and try again.',
+        })
+        setOpen(true)
+        return
+      }
       // Itreate over all the test cases to evaluete the user's code.
       for (const testCase of testCases) {
         const input = testCase.input
@@ -71,11 +114,17 @@ export function CodePage() {
 
           // Compate the result to the expected output
           if (JSON.stringify(result) === JSON.stringify(expectedOutput)) {
-            alert('Correct solution')
+            setModalContent({
+              icon: '😊',
+              text: 'Great job!🎊',
+            })
+            setOpen(true)
           } else {
-            alert(
-              `Incorrect solution. Expected: ${expectedOutput}, Got: ${result}`
-            )
+            setModalContent({
+              icon: 'Incorrect solution 😓',
+              text: 'Keep going, hard work pays off!',
+            })
+            setOpen(true)
           }
         } catch (innerErr) {
           alert(`Error during execution: ${innerErr.message}`)
@@ -112,10 +161,19 @@ export function CodePage() {
           <p>
             <strong>Your role:</strong> {role}
           </p>
-          <p>
-            <strong>Student Present:</strong>{' '}
-            {roomStatus.isStudentPresent ? 'Yes' : 'No'}
-          </p>
+          {role === 'mentor' && (
+            <p>
+              <strong>Student Present:</strong>{' '}
+              {roomStatus.isStudentPresent ? 'Yes' : 'No'}
+            </p>
+          )}
+          {role === 'student' && (
+            <p>
+              <strong>Mentor Present:</strong>{' '}
+              {roomStatus.isMentor ? 'Yes' : 'No'}
+            </p>
+          )}
+
           <p>
             <strong>Viewers:</strong> {roomStatus.watcherCount}
           </p>
@@ -127,6 +185,7 @@ export function CodePage() {
           value={code.starterCode}
           onChange={handleEditorChange}
           theme="vs-dark"
+          options={{ readOnly: role !== 'student' }}
         />
         <div className="btn-container">
           <div className="left-buttons">
@@ -142,6 +201,23 @@ export function CodePage() {
           </button>
         </div>
         {solution && <p className="solution-text">{code.solution}</p>}
+      </div>
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              {modalContent.icon}
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              {modalContent.text}
+            </Typography>
+          </Box>
+        </Modal>
       </div>
     </div>
   )
