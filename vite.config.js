d572df0aeaec8af671f347d44fbf5d73c3399082
Nir Fakiro <fakironir@gmail.com>
2024-11-30 23:28:05 +0200

    Build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../codeTogether-backend/public',
+    emptyOutDir: true,
+  },
+  define: {
+    global: 'window',
+  },
+  global: {},
 })
